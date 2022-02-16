b95991babcf27f1cc26503c41c1e8dae4c5089c4
duldiev <raiymbek132@gmail.com>
2022-02-16 18:54:33 +0600

    Version 1.0

--- PlayersBall/ViewController.swift
+++ PlayersBall/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+
+    @IBOutlet weak var coverImage: UIImageView!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(playButton.currentTitle!)
     }
 
-
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        print(sender.currentTitle!)
+        if sender.currentTitle! == "Play" {
+            sender.setTitle("Stop", for: .normal)
+            playSound(true)
+        } else {
+            sender.setTitle("Play", for: .normal)
+            playSound(false)
+        }
+    }
+    
+    func playSound(_ state: Bool) {
+        let url = Bundle.main.url(forResource: "song", withExtension: "mp3")
+        
+        player = try! AVAudioPlayer(contentsOf: url!)
+        
+        if state {
+            player.play()
+        } else {
+            player.pause()
+        }
+    }
+    
 }
 
